f85c05c10ced9398af4ec4c4383daeaa4a8712c2
Dominik Moser <193428+hemeroc@users.noreply.github.com>
2025-02-26 09:17:08 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         outlook_src = pkgs.fetchurl {
           url = "https://go.microsoft.com/fwlink/p/?linkid=525137";
-          sha256 = "sha256-WETbiVGC+PJqk6HaLGEPUM0+LHDYlcm4IrA9vTulfdw";
+          sha256 = "sha256-idze8QXl2SR4+iiIGw0jLJDjj2QWdMWDmri6Kg8D6lg=";
           curlOptsList = [ "-L" ];
         };
         teams_src = pkgs.fetchurl {
